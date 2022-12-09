322d61da4862d01a34a297857a9fd0026c4d20ec
Haltarys <45515869+Haltarys@users.noreply.github.com>
2022-12-09 16:21:15 +0100

    build: fix typescript build error and ignore build and dependency folders

--- HUB/workshop/ws_ts/ts_web/src/index.ts
+++ HUB/workshop/ws_ts/ts_web/src/index.ts
@@ -1,53 +1,53 @@
 /*
-** Class
-*/
+ ** Class
+ */
 
 interface IPokemon {
-    id: number;
-    name: string;
-    imageSrc: string;
-    shiny: string;
-    type: string; 
-};
+  id: number;
+  name: string;
+  imageSrc: string;
+  shiny: string;
+  type: string;
+}
 
 /*
-** variables
-*/
+ ** variables
+ */
 
-const container: HTMLElement | any = document.getElementById("container");
-const spinner: HTMLElement | any = document.getElementById("spinner");
+const container: HTMLElement | any = document.getElementById('container');
+const spinner: HTMLElement | any = document.getElementById('spinner');
 const pokemonNumber: number = 151;
 const pokemonList: IPokemon[] = [];
 
 /*
-** Core
-*/
+ ** Core
+ */
 
 const getPokemon = async (id: number): Promise<void> => {
-    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then(res => res.json());
-    // console.log(pokemon);
-    const pokemon: IPokemon = {
-        id,
-        name: data.name,
-        imageSrc: data.sprites.front_default,
-        shiny: data.sprites.front_shiny,
-        type: data.types.map((e: any) => e.type.name).join(',')
-    };
-    pokemonList.push(pokemon);
-}
+  const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) => res.json());
+  // console.log(pokemon);
+  const pokemon: IPokemon = {
+    id,
+    name: data.name,
+    imageSrc: data.sprites.front_default,
+    shiny: data.sprites.front_shiny,
+    type: data.types.map((e: any) => e.type.name).join(','),
+  };
+  pokemonList.push(pokemon);
+};
 
 const showSpinner = (): void => {
-    spinner.style.display = "flex";
-    container.style.display = "none";
+  spinner.style.display = 'flex';
+  container.style.display = 'none';
 };
 
 const hideSpinner = (): void => {
-    spinner.style.display = "none";
-    container.style.display = "grid";
+  spinner.style.display = 'none';
+  container.style.display = 'grid';
 };
 
 const showPokemon = (pokemon: IPokemon): void => {
-    const element: string = `
+  const element: string = `
         <div class="card">
             <span class="card-id">#${pokemon.id}</span>
             <img class="card-image" src=${pokemon.imageSrc} alt=${pokemon.name} />
@@ -56,14 +56,14 @@ const showPokemon = (pokemon: IPokemon): void => {
             <span class="card-details">${pokemon.type}</span>
         </div>
     `;
-    container.innerHTML += element;
+  container.innerHTML += element;
 };
 
 (async () => {
-    showSpinner();
-    for (let i: number = 1; i <= pokemonNumber; i++) {
-        await getPokemon(i);
-    }
-    hideSpinner();
-    pokemonList.forEach(pokemon => showPokemon(pokemon));
+  showSpinner();
+  for (let i: number = 1; i <= pokemonNumber; i++) {
+    await getPokemon(i);
+  }
+  hideSpinner();
+  pokemonList.forEach((pokemon) => showPokemon(pokemon));
 })();
